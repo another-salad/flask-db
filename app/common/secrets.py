3c031db3584f5436d04c2a63d9917a408aea439b
jumbobumbo <dwheels91@gmail.com>
2021-01-23 15:49:50 +0000

    read_secrets, file_changes

--- app/common/secrets.py
+++ app/common/secrets.py
@@ -7,6 +7,21 @@ def read_secrets() -> dict:
     """Reads the secret files to fetch the username and password for the DB
 
     Returns:
-        dict: The username and password
+        dict: The DB user's username and password
     """
-    pass
+
+def read_secrets(secret_file_names: list) -> dict:
+    """Reads the file and returns its contents as a string
+
+    Args:
+        secret_file_names (list): A list of filenames from the 'secrets' DIR
+
+    Returns:
+        dict: {"secret_file_name": "contents"}
+    """
+    return_dict = {}
+    for fi in secret_file_names:
+        with open(Path(mod_path, "secrets", fi), "r") as f:
+            return_dict.update({fi : f.read()})
+
+    return return_dict
